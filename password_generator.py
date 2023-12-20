6c431228e8e7ca15267351ec7a144a14ec0fe73d
HamidReza <ataei1564@gmail.com>
2023-12-20 21:47:22 +0330

    add punctuation characters to password

--- password_generator.py
+++ password_generator.py
@@ -5,18 +5,19 @@ import pyperclip
 
 def pass_gen():
     password_lenght = 12
-    alphabet = string.ascii_letters + string.digits + ' $@!?~' 
+    alphabet = string.ascii_letters + string.digits + ' ' + string.punctuation 
     while True:
         password = ''.join(secrets.choice(alphabet) for i in range(password_lenght))
         if (any(c.islower() for c in password)
                 and any(c.isupper() for c in password)
                 and sum(c.isdigit() for c in password) >= 3
-                and any(c.isspace() for c in password[:password_lenght-1])):
+                and any(c.isspace() for c in password[:password_lenght-1])
+                and any(string.punctuation for c in password)):
             break
         
     return password
 
-
+print(string.punctuation)
 password = pass_gen()
 pyperclip.copy(password)
 print('your password is --> ', password)
